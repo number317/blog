0a89612ff5a0ec9dfb4c1df4f169976e9d2e6d8a
number317 <number317@users.noreply.github.com>
2024-12-18 10:12:43 +0000

    deploy: bf548d83649660214498b5309264ad90dc4c6c4b

---
+++ js/single.js
@@ -0,0 +1,47 @@
+function copyCode(elementId, button) {
+  if (button.dataset?.copying === 'true') {
+    return;
+  }
+  button.dataset.copying = 'true';
+  const codeElement = document.getElementById(elementId);
+  const codeText = codeElement.textContent;
+  navigator.clipboard.writeText(codeText)
+    .then(() => {
+      button.setAttribute('title', 'copied');
+      button.innerHTML = '<i class="fa fa-check"></i>';
+      setTimeout(() => {
+        button.setAttribute('title', 'copy');
+        button.innerHTML = '<i class="fa fa-copy"></i>';
+        button.dataset.copying = false;
+      }, 3000);
+    })
+    .catch(err => {
+      console.error("copy code error：", err);
+    });
+}
+
+function addCopyButtonsToIndentedCodeBlocks() {
+  const codeBlocks = document.querySelectorAll('pre code');
+  codeBlocks.forEach((codeBlock) => {
+    if (!codeBlock.closest('.code-block')) {
+      const id = `code-indented-${Math.random().toString(36).substring(2, 15)}`;
+      codeBlock.setAttribute('id', id);
+      const container = document.createElement('div');
+      container.classList.add('code-block');
+      container.classList.add('code-block-container-indented-added');
+      const button = document.createElement('button');
+      button.classList.add('copy-code-button');
+      button.setAttribute('onclick', `copyCode('${id}', this)`);
+      button.setAttribute('title', 'copy');
+      button.innerHTML = '<i class="fa fa-copy"></i>';
+
+
+      const preElement = codeBlock.parentNode;
+      preElement.parentNode.insertBefore(container, preElement);
+      container.appendChild(preElement);
+      container.appendChild(button);
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', addCopyButtonsToIndentedCodeBlocks);
